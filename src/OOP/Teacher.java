564867a5647f8ed0d672dd74ec8b48c0123ded8c
sajjad0057 <sajjadhossain0057@gmail.com>
2021-05-29 01:51:49 +0600

    introduction constructor

--- src/OOP/Teacher.java
+++ src/OOP/Teacher.java
@@ -3,6 +3,15 @@ package OOP;
 public class Teacher {
     String name, gender;
     int phone ;
+    /*
+    A constructor in Java is a special method that is used to initialize objects. The constructor is called when an object of a class is created.
+    It can be used to set initial values for object attributes: In java Class name and constructor name must be same .
+    */
+    Teacher(String n , String g, int p){
+       name = n;
+       gender = g;
+       phone = p;
+    }
 
     void displayInfo(){      // declare a method
         System.out.println("Teacher Name : "+name);
--- src/OOP/Test.java
+++ src/OOP/Test.java
@@ -2,18 +2,11 @@ package OOP;
 
 public class Test {
     public static void main(String[] args) {
-//        Teacher teacher1; // object declare
-//        teacher1 = new Teacher() // create object
-        // declare and create object at a time :
 
-        Teacher teacher1 = new Teacher(); // here declare and create teacher1 object from Teacher Class.
-
-        teacher1.setInfo("sajjad Hossain","male",87458);
+        Teacher teacher1 = new Teacher("sajjad Hossain","male",87458); // here declare and create teacher1 object from Teacher Class.
         teacher1.displayInfo();
 
-        Teacher teacher2 = new Teacher(); // here declare and create teacher1 object from Teacher Class.
-        teacher2.setInfo("xyz","male",8784458);
-
+        Teacher teacher2 = new Teacher("xyz","male",8784458); // here declare and create teacher1 object from Teacher Class.
         teacher2.displayInfo();
 
 
