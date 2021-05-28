8e1bb4698378c9c23e8fac91fe1c3bb1744bf6c5
sajjad0057 <sajjadhossain0057@gmail.com>
2021-05-29 02:22:14 +0600

    Returning Value from Method

---
+++ src/ReturningValueFromMethod/Test.java
@@ -0,0 +1,10 @@
+package ReturningValueFromMethod;
+
+public class Test {
+    public static void main(String[] args) {
+        demo ob1 = new demo();
+        int result = ob1.Square(5);
+        System.out.println("Square Of 5 : "+result);
+
+    }
+}
---
+++ src/ReturningValueFromMethod/demo.java
@@ -0,0 +1,15 @@
+package ReturningValueFromMethod;
+
+public class demo {
+
+    /*
+    return_type method_name(data_type parameter_name){
+
+                        }
+   */
+
+    int Square(int value){
+        return value*value;
+
+    }
+}
