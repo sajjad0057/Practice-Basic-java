863e5165ef4377b110517529a4a3f24a7ab6a461
sajjad0057 <sajjadhossain0057@gmail.com>
2021-06-07 23:31:48 +0600

    Linkedlist 1

---
+++ src/LinkedlistInJava/Linkedlist_demo.java
@@ -0,0 +1,15 @@
+package LinkedlistInJava;
+
+import java.util.LinkedList;
+
+public class Linkedlist_demo {
+    public static void main(String[] args) {
+        LinkedList<String> name = new LinkedList<String>();
+        name.add("sajjad");
+        name.add("arif");
+        name.add("nafiul");
+        name.add("arman");
+        name.add("mahdi");
+        System.out.println("Name : "+name);
+    }
+}
